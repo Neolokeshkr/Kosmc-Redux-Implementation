6d0923c195d81e74cbcbf1542d73378a4c70639e
Lokesh Kumar <neolokeshkr@gmail.com>
2023-09-26 01:17:15 +0530

    mapStateToProps and mapDispatchToProps implemented

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import { useEffect } from "react";
 import { receiveData } from "./action";
 import { connect, useDispatch, useSelector } from "react-redux";
 
-function App({userDataRedux, receiveData}) {
+function App(props) {
 
   // console.log(data)
+  // const dispatch = useDispatch();
   
   useEffect( () => {
     makeRequest() 
   }, []);
   
-  const data = useSelector(state => state.userData)
-  console.log("selector"+ JSON.stringify(data)) 
+  // const data = useSelector(state => state.userData)
+  // console.log(data)
 
   async function makeRequest() {
     try {
@@ -21,16 +22,19 @@ function App({userDataRedux, receiveData}) {
         throw new Error(`Status: ${response.status} Status Text: ${response.statusText}`)
       }
       const userData = await response.json();
-      receiveData(userData)  
-      console.log("useEffect"+ JSON.stringify(userData)) 
+      // dispatch(save(userData));
+      props.save(userData)
+      // console.log(`useEffect ${userData}`) 
     }
     catch (err) {
       console.log(err)
     }
   }
 
+  console.log(props.userDataRedux)
+
   return (
-    <div>Image Application</div>
+    <div>Data Fetch</div>
   );
 }
 
@@ -38,4 +42,8 @@ const mapStateToProps = (state) => ({
   userDataRedux: state.userData
 })
 
-export default  connect(mapStateToProps, { receiveData })(App);
+const mapDispatchToProps = {
+  save: receiveData
+}
+
+export default  connect(mapStateToProps, mapDispatchToProps)(App);
